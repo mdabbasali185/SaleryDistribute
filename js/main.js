ee181cc2ff701bac72b75103cce57816c01165c8
Md Abbas Ali <mdabbasali185@gmail.com>
2022-02-17 02:37:57 +0600

    error-msg

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// calculate button click
+document.getElementById("calculate").addEventListener("click",function(){
+    let incomeTextValue= document.getElementById("income")
+    let foodTextValue = document.getElementById("food")
+    let rentTextValue = document.getElementById("rent")
+    let clothesTextValue=document.getElementById("clothes")
+    
+})
