e63e6bb937101c791d27efc620f6378a46cf2a22
Md Abbas Ali <mdabbasali185@gmail.com>
2022-02-17 12:08:34 +0600

    final commit

--- js/main.js
+++ js/main.js
@@ -1,45 +1,67 @@
 // calculate button click
+function errorFunction(errorText) {
+    document.getElementById("modal_message").innerText = errorText
+    document.getElementById("error-msg").style.display = "block"
+}
+// input clear function
+function inputClear(arr) {
+    let clearInput
+    for (const input of arr) {
+        let inputValue = document.getElementById(input)
+        clearInput = inputValue.value = "";
+    }
+    return clearInput;
+}
+// input inner value
+function inputInnerValue(id) {
+    let textValue = document.getElementById(id)
+    return parseInt(textValue.value)
+}
 let balance = 0;
 document.getElementById("calculate").addEventListener("click", function () {
-    let incomeTextValue = document.getElementById("income")
-    let textNumberValue = parseInt(incomeTextValue.value)
-    let foodTextValue = document.getElementById("food")
-    let countFoodValue = parseInt(foodTextValue.value)
-    let rentTextValue = document.getElementById("rent")
-    let countRentValue = parseInt(rentTextValue.value)
-    let clothesTextValue = document.getElementById("clothes")
-    let countClothesValue = parseInt(clothesTextValue.value)
-    let total= countFoodValue+countFoodValue+countClothesValue
+    let textNumberValue = inputInnerValue("income")
+    let countFoodValue = inputInnerValue("food")
+    let countRentValue = inputInnerValue("rent")
+    let countClothesValue = inputInnerValue("clothes")
+    let total = countFoodValue + countFoodValue + countClothesValue
     // error handeling
     if (isNaN(textNumberValue) || isNaN(countFoodValue) || isNaN(countRentValue) || isNaN(countClothesValue)) {
-        document.getElementById("modal_message").innerText = "fill up this box with number please."
-        document.getElementById("error-msg").style.display = "block"
-    } else if (textNumberValue < 0 || countFoodValue < 0 || countRentValue < 0 || countClothesValue<0){
-        document.getElementById("modal_message").innerText = "Kindly fill up positive numbers."
-        document.getElementById("error-msg").style.display = "block"
-    }else if(textNumberValue<total){
-        document.getElementById("modal_message").innerText = "Expenses larger than income."
-        document.getElementById("error-msg").style.display = "block"
+        errorFunction("fill up this box with number please.")
+
+    } else if (textNumberValue < 0 || countFoodValue < 0 || countRentValue < 0 || countClothesValue < 0) {
+        errorFunction("Kindly fill up positive numbers.")
+
+    } else if (textNumberValue < total) {
+        errorFunction("Expenses larger than income.")
+
     }
-    else{
-        
-        document.getElementById("total-expenses").innerText=total;
-        balance=textNumberValue-total;
-        document.getElementById("balance").innerText=balance;
+    else {
+
+        document.getElementById("total-expenses").innerText = total;
+        balance = textNumberValue - total;
+        document.getElementById("balance").innerText = balance;
     }
-    
+    inputClear(["income", "food", "rent", "clothes"])
 })
 
-document.getElementById("percent-save").addEventListener("click", function(){
+document.getElementById("percent-save").addEventListener("click", function () {
+
     let percentText = document.getElementById("percent")
     let percent = parseInt(percentText.value)
-    let saving = (balance*percent)/100
-    document.getElementById("saving-amount").innerText=saving
-    document.getElementById("remaining-balance").innerText=balance-saving
+    if (percent == "" || isNaN(percent)) {
+        errorFunction("valid number only.")
+    } else if (percent < 0) {
+        errorFunction("positive number only.")
+    }
+    else if (percent > 100) {
+        errorFunction("You can't save more money that you have")
+    } else {
+        let saving = ((balance * percent) / 100).toFixed(2)
+        document.getElementById("saving-amount").innerText = saving
+        document.getElementById("remaining-balance").innerText = (balance - saving).toFixed(2)
+    }
+    inputClear(["percent"])
 })
-
-
-
 document.getElementById('error-msg').addEventListener('click', function () {
     document.getElementById('error-msg').style.display = 'none'
 })
